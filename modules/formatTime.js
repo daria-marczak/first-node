0009930b165f4ea3e88a23abe4718b85e68801db
Daria Marczak <31421134+daria-marczak@users.noreply.github.com>
2017-11-30 16:48:30 +0100

    Update formatTime.js

--- modules/formatTime.js
+++ modules/formatTime.js
@@ -6,8 +6,9 @@ function getFormatTime() {
     var time = Math.floor(uptime) / 60;
     var upTimeMinutes = time.toFixed(0);
 
-    var minutesToHours = upTimeMinutes / 60;
-
+    var toHours = upTimeMinutes / 60;
+    var minutesToHours = toHours.toFixed(0);
+    
     console.log("System uptime is: " + upTimeMinutes + " min, what makes it " + minutesToHours + " hours");
 }
 
