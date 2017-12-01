42ca23671ab6b30f1ea25390650fec32cfec3ec0
Daria Marczak <31421134+daria-marczak@users.noreply.github.com>
2017-12-01 18:35:52 +0100

    Update TimeFormatter.js

--- modules/TimeFormatter.js
+++ modules/TimeFormatter.js
@@ -1,6 +1,6 @@
 var os = require("os");
 
-function getFormatTime() {
+function getFormatTime(uptime) {
     var uptime = os.uptime();
 
     var hours = Math.floor(uptime / 3600);
@@ -9,7 +9,7 @@ function getFormatTime() {
     var minutes = Math.floor(uptime / 60);
     var seconds = (uptime % 60).toFixed(0);
 
-    console.log("System uptime is: " + hours + " h " + minutes + " min " + seconds + " sec.");
+    return(console.log("System uptime is: " + hours + " h " + minutes + " min " + seconds + " sec."));
 }
 
 exports.print = getFormatTime;
