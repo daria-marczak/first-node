06d5b8993be19466415b973a5c4f3e1a83859b3c
Daria Marczak <31421134+daria-marczak@users.noreply.github.com>
2017-12-04 17:44:25 +0100

    Update TimeFormatter.js

--- modules/TimeFormatter.js
+++ modules/TimeFormatter.js
@@ -1,15 +1,11 @@
-var os = require("os");
-
-
 function getFormatTime(time) {
-
     var hours = Math.floor(time / 3600);
     time %= 3600;
 
     var minutes = Math.floor(time / 60);
     var seconds = (time % 60).toFixed(0);
 
-    return (hours + " hours " + minutes + " minutes " + seconds + " seconds.");
+    return hours + " hours " + minutes + " minutes " + seconds + " seconds.";
 }
 
 
